44e7d8ea54ca43db770402a6dadf1260017f9172
Stewart Leach <stewart.leach@isobar.com>
2018-08-06 22:26:26 +1000

    updating product catalog

--- src/Main.java
+++ src/Main.java
@@ -11,11 +11,14 @@ public class Main {
 
         //filter is used to filter things out of a collection, so use inverse to find.
         List<Product> laptops = productCatalog.getProductCatalog()
-                                    .filter(o -> !o.getCategories()
+                                    .filter(o -> o.getCategories()
                                             .stream()
-                                            .anyMatch(category -> category.getName().equals("Laptop")))
+                                            .anyMatch(category -> category.getName().equals("Macbooks")))
                                     .map(Product::getProduct).collect(Collectors.toList());
         System.out.println(laptops);
 
+        System.out.println("List products of a category, including child products");
+
+        System.out.println("Get category path of product");
     }
 }
--- src/ProductCatalog.java
+++ src/ProductCatalog.java
@@ -26,24 +26,42 @@ public class ProductCatalog {
         Category computer = new Category("Computer");
         Category laptop = new Category("Laptop");
         Category tablet = new Category("Tablet");
+        Category macbooks = new Category("Macbooks");
+        Category dell = new Category("Dell");
+        Category lenovo = new Category("Lenovo");
 
-        ArrayList<Category> categories1 = new ArrayList<Category>() {{
+        ArrayList<Category> categoriesMac = new ArrayList<Category>() {{
             add(electronics);
             add(computer);
             add(laptop);
+            add(macbooks);
         }};
 
-        ArrayList<Category> categories2 = new ArrayList<Category>() {{
+        ArrayList<Category> categoriesDell = new ArrayList<Category>() {{
+            add(electronics);
+            add(computer);
+            add(laptop);
+            add(dell);
+        }};
+
+        ArrayList<Category> categoriesLenovo = new ArrayList<Category>() {{
+            add(electronics);
+            add(computer);
+            add(laptop);
+            add(lenovo);
+        }};
+
+        ArrayList<Category> categoriesTablet = new ArrayList<Category>() {{
             add(electronics);
             add(computer);
             add(tablet);
         }};
 
         Stream<Product> sample1 = Stream.of(
-            new Product("Macbook", "Apple Macbook Pro", new Float(3000.00), categories1),
-            new Product("Dell 15", "Dell 15 inch laptop", new Float(2500.00), categories1),
-            new Product("Lenovo 15", "Lenovo 15 inch laptop", new Float(2700.00), categories1),
-            new Product("iPad", "iPad Pro Tablet", new Float(1400.00), categories2)
+            new Product("Apple MacBook Air MJVE2LL/A 13-inch Laptop", "Apple Macbook Pro", new Float(3000.00), categoriesMac),
+            new Product("Dell 15", "Dell 15 inch laptop", new Float(2500.00), categoriesDell),
+            new Product("Lenovo 15", "Lenovo 15 inch laptop", new Float(2700.00), categoriesLenovo),
+            new Product("iPad", "iPad Pro Tablet", new Float(1400.00), categoriesTablet)
         );
 
         return sample1;
